37b32f17460f365cb2a5469df9dfec47f5b62f4c
Christophe Alexandre <chris.perso@gmail.com>
2016-06-28 16:27:43 +0200

    setup config

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='sec-edgar',
+    version='',
+    packages=[''],
+    package_dir={'': 'src'},
+    url='',
+    license='',
+    author='christophe',
+    author_email='',
+    description='', requires=['bs4', 'requests']
+)
